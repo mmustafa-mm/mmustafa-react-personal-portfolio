f783403664a35c777948358ccca1b85dc0afee76
mmustafa-mm <mmustafa@mccneb.edu>
2026-02-13 03:07:11 +0000

    Lab work

--- frontend/__tests__/Button.test.jsx
+++ frontend/__tests__/Button.test.jsx
@@ -1,6 +1,6 @@
 // components/Button.test.jsx
 import { render, screen } from '@testing-library/react'
-import Button from './Button'
+import Button from '../components/Button'
 
 test('renders a Submit button', () => {
   render(<Button label="Submit" />)
---
+++ frontend/jest.setup.js
@@ -0,0 +1,4 @@
+// jest.setup.js
+// Adds jest-dom matchers like `toBeInTheDocument()`
+
+require('@testing-library/jest-dom/extend-expect')
